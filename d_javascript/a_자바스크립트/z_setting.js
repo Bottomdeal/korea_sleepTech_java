8cfdb99540502c9b9e27992bca030842ac3ccd81
Bottomdeal <zpalqm1357@nate.com>
2025-03-13 10:31:53 +0900

    20250313

---
+++ d_javascript/a_자바스크립트/z_setting.js
@@ -0,0 +1,18 @@
+console.log('안녕하세요. 콘솔창에 출력합니다.');
+console.log(123);
+
+console.log();
+
+import moduleName from 'module';
+
+function name(params) {
+
+}
+
+for (const item of object) {
+  
+}
+
+for (const item in object) {
+  
+}
